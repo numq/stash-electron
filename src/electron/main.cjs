8bf262dd153c22e7ed2f8274edde955c1040a1ea
numq <numq.dev@gmail.com>
2022-07-01 21:05:35 +0300

    Provide main.cjs refactoring

--- src/electron/main.cjs
+++ src/electron/main.cjs
@@ -4,28 +4,19 @@ const {app, BrowserWindow} = require("electron");
 
 (() => {
 
-    let mainWindow;
-
-    const createWindow = () => {
+    const createWindow = async showValue => {
         const startUrl = process.env.ELECTRON_START_URL || url.format({
             pathname: path.join(__dirname, '../index.html'),
             protocol: 'file:',
             slashes: true,
         });
-        mainWindow = new BrowserWindow({width: 500, height: 500, minHeight: 500, minWidth: 500});
-        mainWindow.loadURL(startUrl);
-        mainWindow.on('closed', () => {
-            mainWindow = null;
-        });
-
+        const window = new BrowserWindow({width: 500, height: 500, minHeight: 500, minWidth: 500, show: showValue});
+        await window.loadURL(startUrl);
     }
 
-    app.removeAllListeners('ready');
-    app.on('ready', createWindow);
+    app.whenReady().then(() => createWindow(true));
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') app.quit();
     });
-    app.on('activate', () => {
-        if (mainWindow === null) createWindow();
-    });
+
 })();
