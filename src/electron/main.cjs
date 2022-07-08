e6a88a1bd975af138fc99b74648cfad401f0cc94
numq <numq.dev@gmail.com>
2022-07-08 23:15:35 +0300

    Fix electron blank screen

--- src/electron/main.cjs
+++ src/electron/main.cjs
@@ -1,26 +1,25 @@
-const path = require("path");
 const url = require("url");
 const {app, BrowserWindow} = require("electron");
 
 (() => {
 
-    const createWindow = async showValue => {
-        const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '../index.html'),
-            protocol: 'file:',
-            slashes: true,
-        });
+    const createWindow = async () => {
         const window = new BrowserWindow({
             height: 500,
             width: 500,
             minHeight: 500,
             minWidth: 500,
-            show: showValue
+            show: false
+        });
+        const startUrl = process.env.ELECTRON_START_URL || url.format({
+            pathname: `file://${__dirname}/../build/index.html`,
+            protocol: 'file:',
+            slashes: true,
         });
         await window.loadURL(startUrl);
+        window.on('ready-to-show', window.show);
     }
-
-    app.whenReady().then(() => createWindow(true));
+    setTimeout(() => app.whenReady().then(createWindow), 4000);
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') app.quit();
     });
