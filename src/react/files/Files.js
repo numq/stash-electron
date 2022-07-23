c176a548e41db9499a6c68049518e30e329b6b1e
numq <numq.dev@gmail.com>
2022-07-23 18:05:16 +0300

    Provide desktop compatibility fixes

--- src/react/files/Files.js
+++ src/react/files/Files.js
@@ -4,20 +4,31 @@ import {QrCodeImage} from "../qrcode/QrCodeImage.js";
 import {Button, Carousel, Form, Image, Stack} from "react-bootstrap";
 import JSZip from "jszip";
 import * as FileSaver from "file-saver";
+import {ImageFile} from "./ImageFile.js";
 
-export const Content = () => {
+export const Files = () => {
 
     const {uri, client} = useContext(ServiceContext);
     const [images, setImages] = useState([]);
 
+    const urlFromFile = image => {
+        const file = JSON.parse(image);
+        return `data:image/${file.extension};base64,${file.blob}`;
+    };
+
+    const fileFromUrl = url => JSON.stringify(ImageFile(url.split(";")[0].split("/")[1], url.split(",")[1]));
+
     const onClientMessage = useCallback((type, body) => {
         switch (type) {
-            case 'image': {
-                setImages(prevState => prevState.indexOf(body.image) < 0 ? [...prevState, body.image] : prevState);
+            case 'file': {
+                if (body.extension && body.blob) {
+                    const image = JSON.stringify(ImageFile(body.extension, body.blob));
+                    setImages(prevState => prevState.indexOf(image) < 0 ? [...prevState, image] : prevState);
+                }
                 break;
             }
             case 'refresh': {
-                images.forEach(image => client.signal('image', {image: image}));
+                images.forEach(image => client.signal('file', image));
                 break;
             }
             case 'clear': {
@@ -44,7 +55,7 @@ export const Content = () => {
     const processImage = (image, callback) => {
         const reader = new FileReader();
         reader.onload = () => {
-            callback(reader.result);
+            callback(fileFromUrl(reader.result));
         };
         reader.readAsDataURL(image);
     };
@@ -52,40 +63,50 @@ export const Content = () => {
     const processFiles = files => {
         const types = ['image/jpeg', 'image/jpg', 'image/png'];
         [...files].filter(f => types.includes(f.type)).forEach(file => {
-            processImage(file, image => client.signal('image', {image: image}));
+            processImage(file, image => client.signal('file', image));
         });
     };
 
     const clearFiles = () => client.signal('clear');
 
-    const saveAsFile = async file => FileSaver.saveAs(file, Date.now().toString());
+    const saveAsFile = async (extension, file) => FileSaver.saveAs(file, `${Date.now()}.${extension}`);
+
+    const saveSingle = async file => saveAsFile(JSON.parse(file).extension, urlFromFile(file));
 
     const saveMultiple = async data => data.forEach(saveAsFile);
 
     const saveZip = async data => {
         const zip = JSZip();
         const folder = zip.folder(Date.now().toString());
-        data.forEach(image => folder.file(image.name + ".jpg", image.slice(image.indexOf(",") + 1), {base64: true}))
+        data.forEach((file, idx) => {
+            const image = JSON.parse(file);
+            folder.file(`${Date.now()}${idx}.${image.extension}`, image.blob, {base64: true});
+        })
         folder.generateAsync({
             type: "blob",
             compression: "DEFLATE",
             compressionOptions: {
                 level: 6
             }
-        }).then(saveAsFile);
+        }).then(file => saveAsFile("zip", file));
     };
 
     return (
         <Stack direction={"vertical"} style={{height: "100%", width: "auto"}}>
             <Stack direction={"vertical"} style={{alignItems: "center"}}>
                 <Form style={{height: "auto", width: "50%"}}>
-                    <Form.Control type="file" multiple label="Select files or drag them here" onChange={
-                        event => processFiles(event.target.files)
-                    } onDragOver={event => {
+                    <Form.Control type="file" multiple label="Select files or drag them here" onChange={event => {
+                        event.stopPropagation();
+                        event.preventDefault();
+                        processFiles(event.target.files)
+                    }} onDragOver={event => {
+                        event.stopPropagation();
                         event.preventDefault();
                     }} onDragLeave={event => {
+                        event.stopPropagation();
                         event.preventDefault();
                     }} onDrop={event => {
+                        event.stopPropagation();
                         event.preventDefault();
                         processFiles(event.dataTransfer.files);
                     }}/>
@@ -93,7 +114,7 @@ export const Content = () => {
                 <Stack direction={"horizontal"} style={{height: "auto", alignSelf: "center"}}>
                     {
                         images.length > 0 ?
-                            <Stack style={{width: "fit-content", alignItems: "center", justifyContent: "center"}}
+                            <Stack style={{width: "fit-files", alignItems: "center", justifyContent: "center"}}
                                    gap={1}>
                                 <Button variant={"secondary"}
                                         style={{height: "auto", width: "100%", textAlign: "center"}}
@@ -114,9 +135,9 @@ export const Content = () => {
                         <Carousel style={{alignSelf: "center"}} controls={images.length > 1} interval={null}>{
                             Array.from(images).map((image, idx) =>
                                 <Carousel.Item key={Date.now() + idx} style={{height: "auto", width: "auto"}}>
-                                    <Image src={image}
+                                    <Image src={urlFromFile(image)}
                                            style={{height: "75vh", width: "auto"}}
-                                           onClick={() => saveAsFile(image)}/>
+                                           onClick={() => saveSingle(image)}/>
                                 </Carousel.Item>
                             )
                         }
---
+++ src/react/files/ImageFile.js
@@ -0,0 +1,4 @@
+export const ImageFile = (extension, blob) => ({
+    extension: extension,
+    blob: blob
+});
